3bc1a744f800331a597a7aff451a995eb7658572
johnnyslt <ionut.darescu@gmail.com>
2015-01-12 08:10:55 -0200

    fortune: tools: Add changelog generator
    Change-Id: I5c0a714efa30219f85a7ca995ef4ac7d1fee3d81
    Signed-off-by: ishubhamsingh <coolsks94@gmail.com>
    Signed-off-by: Kizziama <kizziama@proton.me>

--- build/tasks/fortune.mk
+++ build/tasks/fortune.mk
@@ -16,6 +16,7 @@ fortune: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(FORTUNE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(FORTUNE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(FORTUNE_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/fortune/tools/generate_json_build_info.sh $(FORTUNE_TARGET_PACKAGE)
+	$(hide) ./vendor/fortune/tools/changelog
 	@echo ""
 	@echo "Package Completed:"
 	@echo ""
