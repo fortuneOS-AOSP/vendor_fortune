81ef7222bdeeef183194b25a2fe510ac7c0bb144
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    fortune: config: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/fortune.mk
+++ config/fortune.mk
@@ -80,6 +80,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Updater
 ifeq ($(filter-out COMMUNITY,$(FORTUNE_BUILD_TYPE)),)
