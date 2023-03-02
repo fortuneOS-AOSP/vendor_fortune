b5abe8f3794c00b6bd459f91d5eadefe2e54024a
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    fortune: config: Conditionally enable dev keys signing
    Change-Id: I1ff4ed7c87f635708fe792a22ea683534a950551

--- config/version.mk
+++ config/version.mk
@@ -23,3 +23,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     org.fortune.codename=$(FORTUNE_CODENAME) \
     org.fortune.revision=$(FORTUNE_REVISION) \
     org.fortune.version=$(FORTUNE_VERSION)
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
