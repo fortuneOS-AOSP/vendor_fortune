a37fa7b31d8174c537a20e532b46403f032940b4
rdx420 <padraramesh420@gmail.com>
2023-04-02 17:30:22 +0530

    fortune: Add Optional Blur support
    Change-Id: I9a080c83309b3d3ac17f21c4a083f2f16472f073

--- config/fortune.mk
+++ config/fortune.mk
@@ -7,6 +7,13 @@
 # Addons
 include vendor/addons/config.mk
 
+# Blur Support
+ifeq ($(TARGET_SUPPORTS_BLUR),true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
 # Extra packages
 PRODUCT_PACKAGES += \
     OmniJaws
