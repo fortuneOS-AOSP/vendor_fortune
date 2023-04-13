dd98cf67ec87e8d4d7825aa13020f84796953f30
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    fortune: config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

--- config/fortune/tools.mk
+++ config/fortune/tools.mk
@@ -7,12 +7,7 @@
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/fortune/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/fortune/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/fortune/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-lineage.sh
-
+    vendor/fortune/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
     vendor/fortune/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
