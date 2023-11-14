f8c063c0524bf34be58f71072fc95a7493911d51
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    fortune: config: support inline signing for U
    For those who have used .android-certs previously, cp .android-certs/* vendor/aosp/signing/keys/
    
    Change-Id: If8553fc9f89cbc0923abdae46e42218df14535bf

--- config/common/sign.mk
+++ config/common/sign.mk
@@ -1,10 +1,10 @@
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/fortune/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/fortune/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/fortune/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/fortune/signing/keys/otakey.x509.pem
 endif
 endif
