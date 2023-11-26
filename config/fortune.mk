557df2d21bfcd8066113baa4a8a95b43abb7135d
Alberto Ponces <ponces26@gmail.com>
2023-11-26 12:53:36 +0100

    fortune: config: Switch to AOSPA's face unlock implementation
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I7b995ddb6fb54ab77e52adc18a5d76b790f566c0

--- config/fortune.mk
+++ config/fortune.mk
@@ -4,6 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Updater
 ifeq ($(filter-out COMMUNITY,$(FORTUNE_BUILD_TYPE)),)
     PRODUCT_PACKAGES += \
