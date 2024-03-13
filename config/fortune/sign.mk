6d0a44a598b84a1c5a86eb60456853296248c8f6
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    fortune: config: Sign every build variant but eng
    Change-Id: Iad382aa2b2242b89014f1d659fdb4e764d0cca67

--- config/fortune/sign.mk
+++ config/fortune/sign.mk
@@ -1,5 +1,5 @@
 # Signing
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/fortune/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/fortune/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
