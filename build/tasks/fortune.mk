45a65bed65b6400b75b1794641c75a320f6ec159
Kizziama <kizziama@proton.me>
2024-08-28 18:27:09 +0000

    fortune: Initial rebrand to fortuneOS
    Change-Id: I0c40af559e94c4af87d3cfaab67b325d064d4f92

---
+++ build/tasks/fortune.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 FortuneOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# -----------------------------------------------------------------
+# FortuneOS OTA update package
+
+FORTUNE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(FORTUNE_VERSION).zip
+
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+
+CL_CYN="\033[36m"
+CL_PRP="\033[35m"
+
+.PHONY: fortune
+fortune: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(FORTUNE_TARGET_PACKAGE)
+	$(hide) $(SHA256) $(FORTUNE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(FORTUNE_TARGET_PACKAGE).sha256sum
+	echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_CYN}
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(FORTUNE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(FORTUNE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(FORTUNE_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(FORTUNE_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}
---
+++ config/fortune.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2024 FortuneOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Gapps
+$(call inherit-product, vendor/gms/products/gms.mk)
+
+# Updater
+# ifeq ($(filter-out COMMUNITY,$(FORTUNE_BUILD_TYPE)),)
+#    PRODUCT_PACKAGES += \
+#        Updater
+
+#PRODUCT_COPY_FILES += \
+#    vendor/fortune/prebuilt/common/etc/init/init.fortune-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.fortune-updater.rc
+#endif
