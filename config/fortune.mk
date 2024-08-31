b5a46388bead42b3bee05b8e1f64ad865806bfe5
Kizziama <kizziama@proton.me>
2024-08-31 00:03:25 +0000

    fortune: config: Inherit GMS repo
    Change-Id: Id4215103aadf31a9be0c5f9bb051a92faf6c63df

--- config/fortune.mk
+++ config/fortune.mk
@@ -26,6 +26,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# GMS
+$(call inherit-product, vendor/gms/products/gms.mk)
+
 # Updater
 ifeq ($(filter-out COMMUNITY,$(FORTUNE_BUILD_TYPE)),)
     PRODUCT_PACKAGES += \
