1179b63b26ed1374698be0cbbfe1b9ef804b8486
Kizziama <kizziama@proton.me>
2024-09-22 05:49:03 +0000

    fortune: build: Define fortune device property
    Change-Id: I41acf7cb25a089c468e706b4f7e632dd3beaadd9

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -20,5 +20,6 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     org.fortune.build.version=$(FORTUNE_BUILD_VERSION) \
     org.fortune.build_date=$(FORTUNE_BUILD_DATE) \
     org.fortune.build_date_utc=$(FORTUNE_BUILD_DATE_UTC) \
+    org.fortune.device=$(FORTUNE_DEVICE) \
     org.fortune.releasetype=$(FORTUNE_RELEASETYPE) \
     org.fortune.version=$(FORTUNE_VERSION)
