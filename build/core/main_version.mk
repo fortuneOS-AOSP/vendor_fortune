7350aef484ad76141c746457b0cd535b959a6136
Kizziama <kizziama@proton.me>
2024-09-22 05:48:15 +0000

    fortune: build: Define fortune version property
    Change-Id: I176fe87d347c1595972b0957ae840c7a740b1a58

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -20,4 +20,5 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     org.fortune.build.version=$(FORTUNE_BUILD_VERSION) \
     org.fortune.build_date=$(FORTUNE_BUILD_DATE) \
     org.fortune.build_date_utc=$(FORTUNE_BUILD_DATE_UTC) \
-    org.fortune.releasetype=$(FORTUNE_RELEASETYPE)
+    org.fortune.releasetype=$(FORTUNE_RELEASETYPE) \
+    org.fortune.version=$(FORTUNE_VERSION)
